21552181c2de7404a95deba9972a954e3e925e71
kHuynh05 <Kenrich6118@gmail.com>
2024-08-21 20:13:22 -0500

    scrapped current temp and condition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+const puppeteer = require('puppeteer')
+const fs = require('fs').promises;
 
-function App() {
-  return (
-    <div className="container">
-        
-    </div>
-  );
-}
+(async () => { 
+	//initiate the browser 
+	const browser = await puppeteer.launch(); 
+ 
+	//create a new in headless chrome 
+	const page = await browser.newPage(); 
+ 
+	//go to target website 
+	await page.goto('https://www.weather.com/weather/today/l/29.7106,-95.5963', { 
+		//wait for content to load 
+		waitUntil: 'networkidle0', 
+	}); 
+  
+  await page.waitForSelector('[class="CurrentConditions--tempValue--MHmYY"]', { timeout: 10000 });
 
-export default App;
+  // Extract temperature and weather condition
+  const weatherData = await page.evaluate(() => {
+    const temperatureElement = document.querySelector('.CurrentConditions--tempValue--MHmYY');
+    const conditionElement = document.querySelector('.CurrentConditions--phraseValue--mZC_p');
+
+      return {
+          temperature: temperatureElement ? temperatureElement.textContent : 'N/A',
+          condition: conditionElement ? conditionElement.textContent : 'N/A'
+      };
+  });
+
+  console.log('Temperature:', weatherData.temperature);
+  console.log('Weather Condition:', weatherData.condition);
+	//get full page html 
+	const html = await page.content(); 
+ 
+	//store html content in the reactstorefront file 
+	await fs.writeFile('weather.html', html); 
+ 
+	//close headless chrome 
+	await browser.close(); 
+})();
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_APO_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '6ad9b6bdbemshddbeecc1977fe69p13827fjsn22b3b09a9f83',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0087431a96f58e4603cc3e10bae8d2d5";
